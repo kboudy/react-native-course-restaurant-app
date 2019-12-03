768b09101d9c9b08f3c57f10dfe3d330ccb17907
Keith Boudreau <kboudy@gmail.com>
2019-12-03 14:13:41 -0500

    finished 87

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+function SearchScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Search Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {}
+});
+
+export default SearchScreen;
