a8bdbab97710844539d7813ab76cf7397cf29872
Keith Boudreau <kboudy@gmail.com>
2019-12-05 09:29:29 -0500

    finished with 110

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, Text, View } from 'react-native';
 
 function ResultsDetail({ result }) {
   return (
